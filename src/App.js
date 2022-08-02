686418ec1f28ab9da58b2099a62d12aca86a46d4
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-08-03 00:23:34 +0300

    simple counter as a first react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.reset = props.counter
+    this.state = {
+      num: props.counter
+    }
+  }
+
+  getRnd = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  rndBtn = () => {
+    this.setState({
+      num: this.getRnd(-50, 50)
+    })
+  }
+
+  resetBtn = () => {
+    this.setState({
+      num: this.reset
+    })
+  }
+
+  nextNum = () => {
+    const { num } = this.state;
+    (num >= 50) ? this.setState({
+      num: num
+    })
+      : this.setState({
+        num: num + 1
+      })
+  }
+
+  prevNum = () => {
+    const { num } = this.state;
+    (num <= -50) ? this.setState({
+      num: num
+    }) : this.setState({
+      num: num - 1
+    })
+  }
+
+  render() {
+    const { num } = this.state;
+    return (
+      <div className="app">
+        <div className="counter">{num}</div>
+        <div className="controls">
+          <button onClick={this.nextNum}>INC</button>
+          <button onClick={this.prevNum}>DEC</button>
+          <button onClick={this.rndBtn}>RND</button>
+          <button onClick={this.resetBtn}>RESET</button>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
